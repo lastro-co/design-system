9c2038eeefe6a86fc16948dfa1cb9b291ee571ea
Fernando Jesus <fernando.rodrigodejesus@gmail.com>
2026-03-11 20:08:24 -0300

    chore: export conts

--- src/components/ui/Calendar/index.ts
+++ src/components/ui/Calendar/index.ts
@@ -1,3 +1,8 @@
 export { Calendar, CalendarDayButton } from "./Calendar";
+export {
+  MONTHS_PT_BR,
+  MONTHS_PT_BR_SHORT,
+  WEEKDAYS_PT_BR,
+} from "./constants";
 export type { MonthYearPickerProps } from "./MonthYearPicker";
 export { MonthYearPicker } from "./MonthYearPicker";
